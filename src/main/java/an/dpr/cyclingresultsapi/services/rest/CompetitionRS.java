e01bc27294d7c4783e340a0728d336e470503582
rikidpr <saezmr@gmail.com>
2015-07-24 13:42:01 +0200

    bugfixed: solo necesitan carga de etapas/clasificaciones las que tipo STAGES

--- src/main/java/an/dpr/cyclingresultsapi/services/rest/CompetitionRS.java
+++ src/main/java/an/dpr/cyclingresultsapi/services/rest/CompetitionRS.java
@@ -384,10 +384,12 @@ public class CompetitionRS {
      */
     private boolean isNeedLoadStagesAndClassifications(Competition comp) {
 	boolean ret = false;
-	if (dao.getCompetitionClassifications(comp).size()==0 || dao.getCompetitionStages(comp).size()==0){
-	    ret = true;
-	} else if (!competitionFinalizada(comp)){
-	    ret = true;
+	if (comp.getCompetitionType().equals(CompetitionType.STAGES)){
+	    if (dao.getCompetitionClassifications(comp).size()==0 || dao.getCompetitionStages(comp).size()==0){
+		ret = true;
+	    } else if (!competitionFinalizada(comp)){
+		ret = true;
+	    }
 	}
 	return ret;
     }
